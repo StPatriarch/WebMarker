a6966b1d16c3894b69485a22d9f656e67eac2000
stpatriarch <chinaryannarek@gmail.com>
2022-11-09 16:20:36 +0400

    Մեկնաբանությունների լրացումներ։

--- moduls/commands.py
+++ moduls/commands.py
@@ -3,10 +3,18 @@ from moduls import database
 import sys
 import datetime
 
+''' Մոդուլի այս հատվածը հավելվածի աբսրտակցիայի երկրորդ մակարդակն է։ Քանի որ ՏԲ հետ հիմնական կապը արդեն 
+իսկ հաստատված է, այդ հատվածի դերը և ֆունկցիան կայանում է հիմնական և ձևատիպային հարցումների կազմությունը 
+և տվյալների գրանցման կատարումը ՏԲ-ում։ Ուշադրություն դարձեք որ, այս հատվածի պատասխանատվությունը 
+սահմանափակված է հենց իր կոդի ներսում, այն չի փորձում 'հասկանալ' կամ մոդիֆիկացնել մոդուլի մյուս հատվածը այն 
+ուղղակի վերջնում է այդ մոդուլի պատրաստի լուծումները և կատարում է իրեն բաժին ընկած աշխատանքը։
+'''
+
 
 db = database.DbManager('shopdb')
 
 
+# Կատարում է աղյուսակի ստեղծման, անհրաժեշտ սունյակների ստեղծուման հրամանաը։
 class CreateTableCommand:
     def execute(self):
         db.db_table_maker('bookmarker', {
@@ -17,6 +25,7 @@ class CreateTableCommand:
             'added_date': 'TEXT NOT NULL'})
 
 
+# Կատարում է նոր մարքի գրանցմումը ավելացնելով գրանցման ժամանակը։
 class AddBookmarkCommand:
     def execute(self, data):
         data['added_date'] = datetime.datetime.utcnow().isoformat()
@@ -24,6 +33,7 @@ class AddBookmarkCommand:
         return 'bookmark is added!'
 
 
+# Կատարում է գրանցված մարքերի ցուցակը, դասավորում այն ըստ տրված չափանիշի։
 class SelectBookmarkCommand:
     def __init__(self, ordered_by='added_date'):
         self.ordered_by = ordered_by
@@ -32,12 +42,14 @@ class SelectBookmarkCommand:
         return db.to_select('bookmarker', order_by=self.ordered_by)
 
 
+# Կատարում է մարքի ջնջումը ըստ նրա ինդենտիֆիկացոն համարի։
 class DeleteBookmarkCommand:
     def execute(self, data):
         db.to_delete('bookmarker', {'id': data})
         return 'Bookmark is deleted!'
 
 
+# կատարում է հավելվածից դուրս գալու հրամանաը։
 class QuitCommand:
     def execute(self):
         sys.exit()
